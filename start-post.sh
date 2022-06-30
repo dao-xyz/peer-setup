bc9681a2bf3c391ef246b9df671aa4bca1badd8a
Marcus Pousette <marcus.pousette@quantleaf.com>
2022-06-30 11:09:30 +0200

    add echoes

--- start-post.sh
+++ start-post.sh
@@ -2,4 +2,6 @@
 ./setup.sh
 
 # Start Shorbit
+echo "Starting client"
 sudo docker run -p 4001:4003 -p 4001:4003/udp -p 127.0.0.1:8080:8080 -p 127.0.0.1:5001:5001 daoxyz/social-peer
+echo "--- Completed ---"
