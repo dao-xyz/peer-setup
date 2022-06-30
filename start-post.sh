ca639536a426f5f5a2cc4aa2977dbb04e9fe7357
Marcus Pousette <marcus.pousette@quantleaf.com>
2022-06-30 14:41:51 +0200

    ref

--- start-post.sh
+++ start-post.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -e
 
+# Docker
 sudo sh ./setup-docker.sh
 
 # IPFS and NGINX
-sudo sh ./setup.sh
+sudo sh ./setup-ipfs.sh
 
 # Start Shorbit
 echo "Starting client"
 #-p 4001:4003 -p 4001:4003/udp -p 127.0.0.1:8080:8080 -p 127.0.0.1:5001:5001
-sudo docker run daoxyz/social-peer
+docker_ipfs_ip=$(sudo docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ipfs_host)
+sudo docker run -e IPFS_IP=$docker_ipfs_ip daoxyz/social-peer 
 echo "--- Completed ---"
