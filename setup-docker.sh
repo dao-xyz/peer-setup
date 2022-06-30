5edeff5e8d4ae33b1f63abf11c5b0251aaed3131
Marcus Pousette <marcus.pousette@quantleaf.com>
2022-06-30 13:23:53 +0200

    docker setup

--- setup-docker.sh
+++ setup-docker.sh
@@ -5,18 +5,17 @@ if [ -x "$(command -v docker)" ]; then
 else
     echo "Docker is not installed, installing"
     # command
-    sudo apt-get install -y \
-        apt-transport-https \
+    sudo apt-get update
+    sudo apt-get -y install \
         ca-certificates \
         curl \
-        software-properties-common 
-    curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add - 
-    sudo add-apt-repository \
-        "deb https://apt.dockerproject.org/repo/ \
-        ubuntu-$(lsb_release -cs) \
-        main" 
+        gnupg \
+        lsb-release
+        sudo mkdir -p /etc/apt/keyrings
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+        echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
-    sudo apt-get -y install docker-engine 
-    # add current user to docker group so there is no need to use sudo when running docker
-    sudo usermod -aG docker $(whoami)
+    sudo apt-get -y install docker-ce docker-ce-cli
 fi
--- start-post.sh
+++ start-post.sh
@@ -7,5 +7,6 @@ sudo sh ./setup.sh
 
 # Start Shorbit
 echo "Starting client"
-sudo docker run -p 4001:4003 -p 4001:4003/udp -p 127.0.0.1:8080:8080 -p 127.0.0.1:5001:5001 daoxyz/social-peer
+#-p 4001:4003 -p 4001:4003/udp -p 127.0.0.1:8080:8080 -p 127.0.0.1:5001:5001
+sudo docker run daoxyz/social-peer
 echo "--- Completed ---"
