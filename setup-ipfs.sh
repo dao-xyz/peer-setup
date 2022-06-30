e6e600a4ae3a34fee92183dec52b0171a1ddd9d9
Marcus Pousette <marcus.pousette@quantleaf.com>
2022-06-30 17:19:52 +0200

    updat enginx conf

--- setup-ipfs.sh
+++ setup-ipfs.sh
@@ -6,14 +6,24 @@ domain=$(curl -X POST "https://bfbbnhwpfj2ptcmurz6lit4xlu0vjajw.lambda-url.us-ea
    -H "Content-Type: application/json" \
    -d '"'$ipv4'"' | jq -r '.domain') 
 
-mkdir user_conf.d && cp nginxtempl.conf user_conf.d/default.conf
+if [ -z "$domain" ] ; then
+  echo "Failed to obtain domain name"
+  exit 1
+fi
+
+mkdir -p user_conf.d && cp nginx-template.conf user_conf.d/default.conf
 echo "" > user_conf.d/default.conf.original
 sed -i user_conf.d/default.conf.original  -e 's/%DOMAIN%/'$domain'/g' user_conf.d/default.conf
 
 # IPFS
 export ipfs_staging=$(pwd)/ipfs/staging
 export ipfs_data=$(pwd)/ipfs/data
-sudo docker run -d --name ipfs_host -e IPFS_PROFILE=server -v $ipfs_staging:/export -v $ipfs_data:/data/ipfs -p 4001:4001 -p 127.0.0.1:8080:8080 -p 127.0.0.1:5001:5001 ipfs/go-ipfs:latest daemon --enable-pubsub-experiment
+sudo docker run -d --name ipfs_host -e IPFS_PROFILE=server -v $ipfs_staging:/export -v $ipfs_data:/data/ipfs -p 4001:4001 -p 127.0.0.1:8080:8080 -p 127.0.0.1:8081:8081  -p 127.0.0.1:5001:5001 ipfs/go-ipfs:latest daemon --enable-pubsub-experiment
+sudo docker exec ipfs_host ipfs bootstrap rm --all
+sudo docker exec ipfs_host ipfs config Addresses.Swarm '["/ip4/0.0.0.0/tcp/4001", "/ip4/0.0.0.0/tcp/8081/ws", "/ip6/::/tcp/4001"]' --json
+sudo docker stop ipfs_host
+sudo docker start ipfs_host
+
 docker_ipfs_ip=$(sudo docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ipfs_host)
 
 if [ -z "$docker_ipfs_ip" ] ; then
--- start-post.sh
+++ start-post.sh
@@ -10,5 +10,5 @@ sudo sh ./setup-ipfs.sh
 echo "Starting client"
 #-p 4001:4003 -p 4001:4003/udp -p 127.0.0.1:8080:8080 -p 127.0.0.1:5001:5001
 docker_ipfs_ip=$(sudo docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ipfs_host)
-sudo docker run -e IPFS_IP=$docker_ipfs_ip daoxyz/social-peer 
+sudo docker run -d -e IPFS_IP=$docker_ipfs_ip --name post-peer daoxyz/social-peer 
 echo "--- Completed ---"
