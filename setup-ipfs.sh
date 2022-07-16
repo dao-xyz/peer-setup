ed2b5f9b51614e80082a969061e86f42a6532072
Marcus Pousette <marcus.pousette@quantleaf.com>
2022-07-16 20:27:47 +0200

    disable public gateway

--- setup-ipfs.sh
+++ setup-ipfs.sh
@@ -20,7 +20,7 @@ export ipfs_data=$(pwd)/ipfs/data
 sudo docker run -d --name ipfs_host -v $ipfs_staging:/export -v $ipfs_data:/data/ipfs -e IPFS_PROFILE=server --net=host ipfs/go-ipfs:latest daemon --enable-pubsub-experiment --migrate --routing=none
 sleep 10s
 sudo docker exec ipfs_host ipfs bootstrap rm --all
-sudo docker exec ipfs_host ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
+#sudo docker exec ipfs_host ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
 sudo docker exec ipfs_host ipfs config Addresses.Swarm '["/ip4/0.0.0.0/tcp/4001", "/ip4/0.0.0.0/tcp/8081/ws", "/ip6/::/tcp/4001"]' --json
 sudo docker exec ipfs_host ipfs config Discovery.MDNS '{
       "Enabled": false
@@ -29,6 +29,9 @@ sudo docker stop ipfs_host
 sudo docker start ipfs_host
 
 export ipfs_id=$(sudo docker exec ipfs_host ipfs id | jq -r ".ID") 
+
+
+sed -i user_conf.d/default.conf.original  -e 's/%IPFS_ID%/'$ipfs_id'/g' user_conf.d/default.conf
 
 # rm original conf of nginx before starting (to prevent it from beeing used)
 rm user_conf.d/default.conf.original
